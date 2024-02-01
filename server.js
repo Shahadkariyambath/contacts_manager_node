bb7ce6193e9b798a7d9a64ea2def454f70c698f9
shahadkariyambath <shahadchikku@gmail.com>
2024-02-01 13:02:46 +0000

    Express Router & Contacts CRUD Route Setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get();
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
