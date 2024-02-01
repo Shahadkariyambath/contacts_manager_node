2a0e338e863ecaca0b4edda3174bfe3fddbb600e
shahadkariyambath <shahadchikku@gmail.com>
2024-02-01 13:29:33 +0000

    Create Contact Controller for Contacts CRUD Operations

---
+++ controllers/contactControllers.js
@@ -0,0 +1,47 @@
+//@desc Get all conatcts
+//@routes GET /api/contacts
+//@access public
+
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+};
+
+//@desc create new conatcts
+//@routes POST /api/contacts
+//@access public
+
+const createContacts = (req, res) => {
+  res.status(201).json({ message: "Create contact" });
+};
+
+//@desc Get  conatcts
+//@routes Get /api/contacts/:id
+//@access public
+
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get contacts for ${req.params.id}` });
+};
+
+//@desc update new conatcts
+//@routes PUT /api/contacts/:id
+//@access public
+
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `update contacts for ${req.params.id}` });
+};
+
+//@desc delete new conatcts
+//@routes DELETE /api/contacts/:id
+//@access public
+
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: `Delete contacts for ${req.params.id}` });
+};
+
+module.exports = {
+  getContacts,
+  createContacts,
+  getContact,
+  updateContact,
+  deleteContact,
+};
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getContacts,
+  createContacts,
+  getContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactControllers");
 
-router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
-});
+router.route("/").get(getContacts);
 
-router.route("/").post((req, res) => {
-  res.status(200).json({ message: "Create contact" });
-});
+router.route("/").post(createContacts);
 
-router.route("/:id").get((req, res) => {
-  res.status(200).json({ message: `Get contacts for ${req.params.id}` });
-});
+router.route("/:id").get(getContact);
 
-router.route("/:id").put((req, res) => {
-  res.status(200).json({ message: `update contacts for ${req.params.id}` });
-});
+router.route("/:id").put(updateContact);
 
-router.route("/:id").delete((req, res) => {
-  res.status(200).json({ message: `Delete contacts for ${req.params.id}` });
-});
+router.route("/:id").delete(deleteContact);
 
 module.exports = router;
