de345ae6e554876599babfb68ff2150198a9148c
shahadkariyambath <shahadchikku@gmail.com>
2024-02-02 20:22:53 +0000

    Set up mongoose Schema for Contacts

---
+++ models/contactModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const conatctSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add the contact name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add the contact email address"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Please add the contact phone number"],
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+module.exports = mongoose.model("Contact", conatctSchema);
