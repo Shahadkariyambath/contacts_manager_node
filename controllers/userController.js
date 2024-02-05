a335f62e511b5fc8a466a4ebaa2ca490cf5a5325
shahadkariyambath <shahadchikku@gmail.com>
2024-02-05 20:33:16 +0000

    Set up JWT access token for user login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
-const { use } = require("../routes/contactRoutes");
+const jwt = require("jsonwebtoken");
 
 //@desc Register a user
 //@routes POST /api/users/register
@@ -11,7 +11,7 @@ const registerUser = asyncHandler(async (req, res) => {
   //Check if user exists
   if (!username || !email || !password) {
     res.status(404);
-    throw new Error("All fields the mandatory");
+    throw new Error("All fields the mandatory!");
   }
   const userAvailable = await User.findOne({ email });
   if (userAvailable) {
@@ -45,7 +45,31 @@ const registerUser = asyncHandler(async (req, res) => {
 //@routes POST /api/users/login
 //@access public
 const loginrUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login user" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const user = await User.findOne({ email });
+  // compare
+  if (user || (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECERT,
+      { expiresIn: "10m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
 });
 
 //@desc Register a user
