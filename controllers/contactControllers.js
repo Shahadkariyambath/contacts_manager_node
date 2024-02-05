00dce79fda2cd7ee6a880d1f390fad630d0bebc3
shahadkariyambath <shahadchikku@gmail.com>
2024-02-05 22:08:20 +0000

    Implement authentication for CRUD operations and Restrict users to update and delete only their own contacts

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,18 +1,17 @@
 const asyncHandler = require("express-async-handler");
 const Contact = require("../models/contactModel");
+
 //@desc Get all contacts
 //@routes GET /api/contacts
-//@access public
-
+//@access private
 const getContacts = asyncHandler(async (req, res) => {
-  const contacts = await Contact.find();
+  const contacts = await Contact.find({ user_id: req.user.id });
   res.status(200).json(contacts);
 });
 
 //@desc create new contacts
 //@routes POST /api/contacts
-//@access public
-
+//@access private
 const createContacts = asyncHandler(async (req, res) => {
   console.log("This is a request body :", req.body);
   const { name, email, phone } = req.body;
@@ -26,6 +25,7 @@ const createContacts = asyncHandler(async (req, res) => {
     name,
     email,
     phone,
+    user_id: req.user.id,
   });
 
   res.status(201).json(contacts);
@@ -33,8 +33,7 @@ const createContacts = asyncHandler(async (req, res) => {
 
 //@desc Get  contacts
 //@routes Get /api/contacts/:id
-//@access public
-
+//@access private
 const getContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
   if (!contact) {
@@ -46,13 +45,17 @@ const getContact = asyncHandler(async (req, res) => {
 
 //@desc update new contacts
 //@routes PUT /api/contacts/:id
-//@access public
-
+//@access private
 const updateContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
   if (!contact) {
     res.status(404);
     throw new Error("Contact not found");
+  }
+
+  if (contact.user_id.toString() != req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contacts");
   }
   const updateContact = await Contact.findByIdAndUpdate(
     req.params.id,
@@ -64,15 +67,20 @@ const updateContact = asyncHandler(async (req, res) => {
 
 //@desc delete new contacts
 //@routes DELETE /api/contacts/:id
-//@access public
-
+//@access private
 const deleteContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
   if (!contact) {
     res.status(404);
     throw new Error("Contact not found");
   }
-  await Contact.findOneAndRemove(req.params.id);
+
+  if (contact.user_id.toString() != req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to delete other user contacts");
+  }
+
+  await Contact.deleteOne({ _id: req.params.id });
   res.status(200).json(contact);
 });
 
